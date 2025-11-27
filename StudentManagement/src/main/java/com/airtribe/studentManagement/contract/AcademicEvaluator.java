f842e7b7ecbc4a6e4ef907b1b0f80cdecc027fe1
B-SHIKHAR <48488949+B-SHIKHAR@users.noreply.github.com>
2025-11-27 08:24:14 +0530

    Delete StudentManagement directory

--- StudentManagement/src/main/java/com/airtribe/studentManagement/contract/AcademicEvaluator.java
+++
@@ -1,6 +0,0 @@
-package main.java.com.airtribe.studentManagement.contract;
-
-public interface AcademicEvaluator {
-    double calculateGrade(); // contract for dynamic dispatch
-    String getLevel(); // undergraduate/graduate
-}
